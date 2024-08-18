7aba9bc0688704647d0ed0acca9b0663e341596d
Maxim Belinin <akkakiy13@gmail.com>
2024-08-18 17:53:24 +0300

    корректировка тестов

--- tests/test_feed_order.py
+++ tests/test_feed_order.py
@@ -1,5 +1,4 @@
 import allure
-import time
 
 
 @allure.suite('Проверки ленты заказов')
@@ -15,9 +14,9 @@ class TestFeedOrder:
         feed_order_page.full_find_and_transfer_ingredient_bun()
         feed_order_page.wait_find_modal_window()
         feed_order_page.wait_station_text()
-        feed_order_page.find_exit()
-        feed_order_page.get_number_order_modal_window()
-        feed_order_page.click_exit()
+        feed_order_page.find_exit()                             # по непонятным причинам закрыть модальное окно,
+        feed_order_page.get_number_order_modal_window()         # в этом тесте и ниже,
+        feed_order_page.click_exit()                            # удалось только таким образом
         feed_order_page.find_and_click_button_account()
         feed_order_page.find_and_click_button_history_order()
         number_order = feed_order_page.get_number_order_in_history_orders()
